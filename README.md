85e64acd4919c34a15bfe8c7957e059d2acc8c63
miya0001 <miya@wpist.me>
2017-10-09 00:13:44 +0900

    rename

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# wpcf7-thanks-addon
+# wpcf7-redirect-addon
+
+
 
 ## Usage
 
 ```
-[contact-form-7 id="4" title="Contact form 1" thanks="http://wp.test/archives/1"]
+[contact-form-7 id="4" title="Contact form 1" goto="http://wp.test/archives/1"]
 ```
--- redirect-for-contact-form-7.php
+++ redirect-for-contact-form-7.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Cf7 Thanks
  * Plugin URI:      https://github.com/tarosky/wpcf7-thanks-addon
- * Description:     An add-on plugin for Contact Form 7 which redirects to thanks page.
+ * Description:     An add-on plugin for the Contact Form 7 which redirects to the specific page.
  * Author:          Takayuki Miyauchi
  * Author URI:      https://tarosky.co.jp/
  * Text Domain:     cf7-thanks
@@ -20,8 +20,8 @@ add_action( 'plugins_loaded', function() {
 	remove_shortcode( 'contact-form-7' );
 	remove_shortcode( 'contact-form' );
 
-	add_shortcode( 'contact-form-7', 'custom_cf7_shortcode' );
-	add_shortcode( 'contact-form', 'custom_cf7_shortcode' );
+	add_shortcode( 'contact-form-7', '__cf7_shortcode' );
+	add_shortcode( 'contact-form', '__cf7_shortcode' );
 }, 11 );
 
 add_action( 'wpcf7_mail_sent', function() {
@@ -31,9 +31,9 @@ add_action( 'wpcf7_mail_sent', function() {
 	}
 } );
 
-function custom_cf7_shortcode( $atts, $content = null, $code = '' ) {
+function __cf7_shortcode( $atts, $content = null, $code = '' ) {
 	add_filter( 'wpcf7_form_hidden_fields', function() use ( $atts ) {
-		return array( '__goto' => $atts['thanks'] );
+		return array( '__goto' => $atts['goto'] );
 	} );
 	return wpcf7_contact_form_tag_func( $atts, $content, $code );
 }
