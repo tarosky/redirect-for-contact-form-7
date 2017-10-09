d2fd27ef343010ddc9ae81252c7a40727b728870
miya0001 <miya@wpist.me>
2017-10-09 22:54:47 +0900

    update env variable for plugin

--- bin/install-plugin.sh
+++ bin/install-plugin.sh
@@ -2,14 +2,6 @@
 
 set -ex
 
-download() {
-    if [ `which curl` ]; then
-        curl -s "$1" > "$2";
-    elif [ `which wget` ]; then
-        wget -nv -O "$2" "$1"
-    fi
-}
-
-download $CF7 cf7.zip
-unzip cf7.zip -d .lib
-rm -f cf7.zip
+curl -s $WP_PLUGIN -o plugin.zip
+unzip plugin.zip -d .plugin
+rm -f plugin.zip
